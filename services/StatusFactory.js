c3fc9da74449be10a614e0c5015fbab1dbfd893b
John Franti & Kyle Bulloch <student@epicodus.com>
2015-04-22 16:44:22 -0700

    Add StatusController, StatusFactory

---
+++ services/StatusFactory.js
@@ -0,0 +1,9 @@
+spacePirates.factory('StatusFactory', function StatusFactory() {
+  var factory = {};
+
+  factory.life_support = "Nominal";
+  factory.sail = "Undeployed";
+  factory.lifeform = 2;
+  
+  return factory;
+});
