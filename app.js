7630c40458330b5d4b310f6640a4478f14446a28
John Franti & Kyle Bulloch <student@epicodus.com>
2015-04-22 14:42:38 -0700

    Add start.html, and route in app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var spacePirates = angular.module('spacePirates', ['ui.router']);
+
+spacePirates.config(function($stateProvider) {
+
+  $stateProvider.state('start', {
+    url: '',
+    templateUrl: 'partials/start.html'
+  });
+
+});
